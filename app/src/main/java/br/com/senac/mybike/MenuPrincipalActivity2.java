4b1bfdc57e37b657926c59b72c934a0bdb3d61b6
yarosvinicus <yaros.soave@gmail.com>
2022-06-30 16:26:06 -0300

    criando card view

---
+++ app/src/main/java/br/com/senac/mybike/MenuPrincipalActivity2.java
@@ -0,0 +1,16 @@
+
+
+package br.com.senac.mybike;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuPrincipalActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_principal2);
+    }
+}
